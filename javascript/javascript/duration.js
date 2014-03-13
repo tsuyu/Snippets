5e3355c05d6f1bce93aa740b5cdec3977ace4fed
tsuyu <tsuyu@users.noreply.github.com>
2014-03-13 14:48:10 +0800

    Update duration.js

--- javascript/javascript/duration.js
+++ javascript/javascript/duration.js
@@ -18,3 +18,38 @@ startDate = new Date(startDate.getTime() + 1*24*60*60*1000);
 }
 
 showDates(1999,12,1,2000,12,25);
+
+
+   var valid = 0;
+   var valid_past = 0;
+   var from_date = form1.date_from.value; 
+ 
+   
+   var YYYY = from_date.substring(6, 10);
+   var MM = from_date.substring(3, 5);
+   var DD = from_date.substring(0,2);
+   
+   var eventDate1 =new Date(parseInt(YYYY, 10), parseInt(MM, 10) - 1, parseInt(DD, 10));
+   var eventDate = new Date(parseInt(YYYY, 10), parseInt(MM, 10) - 1, parseInt(DD, 10));
+   var today = new Date().getTime();
+   eventDate.setDate(eventDate.getDate()-14);
+   
+   if( eventDate.getTime() < today && eventDate1.getTime() > today){
+	     valid = 1;
+   }else{
+	     valid = 0;
+   }
+   
+   var to_date = form1.date_to.value;
+   
+   var YYYY2 = to_date.substring(6, 10);
+   var MM2 = to_date.substring(3, 5);
+   var DD2 = to_date.substring(0,2);
+   
+   var tod = new Date(parseInt(YYYY2, 10), parseInt(MM2, 10) - 1, parseInt(DD2, 10));
+   
+   if(eventDate1.getTime()<=tod.getTime()){
+	   valid_past = 1;
+   }else{
+	   valid_past = 0;
+   }
