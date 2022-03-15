b3c0b5697567c80a192067d8373fbf2c0568b5cc
Mohamad Yusuf <mohamad.yusuf@hotmail.com>
2022-03-15 14:50:06 +0800

    location

---
+++ php/location.php
@@ -0,0 +1,16 @@
+function getDistanceBetweenPointsNew($latitude1, $longitude1, $latitude2, $longitude2, $unit = 'miles') {
+  $theta = $longitude1 - $longitude2; 
+  $distance = (sin(deg2rad($latitude1)) * sin(deg2rad($latitude2))) + (cos(deg2rad($latitude1)) * cos(deg2rad($latitude2)) * cos(deg2rad($theta))); 
+  $distance = acos($distance); 
+  $distance = rad2deg($distance); 
+  $distance = $distance * 60 * 1.1515; 
+  switch($unit) { 
+    case 'miles': 
+      break; 
+    case 'kilometers' : 
+      $distance = $distance * 1.609344; 
+  } 
+  return (round($distance,2)); 
+}
+
+$query = "SELECT *, (((acos(sin((".$latitude."*pi()/180)) * sin((`latitude`*pi()/180)) + cos((".$latitude."*pi()/180)) * cos((`latitude`*pi()/180)) * cos(((".$longitude."- `longitude`)*pi()/180)))) * 180/pi()) * 60 * 1.1515) as distance FROM `table` WHERE distance <= ".$distance."
